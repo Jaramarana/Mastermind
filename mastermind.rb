9f0742b2944ec6e3bf8c5816d4e9a03ed6dc3168
Adam <ammoclip@gmail.com>
2020-07-02 18:01:31 +0800

    Functional

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,6 @@
 =begin
+I need to work out a way to add the red and white peg things for the codes.
+
 
 =end
 
@@ -11,7 +13,7 @@ class String
     def blue;           "\e[34;1m#{self}\e[0m" end
     def magenta;        "\e[35;1m#{self}\e[0m" end
     def cyan;           "\e[36;1m#{self}\e[0m" end
-    def gray;           "\e[37m#{self}\e[0m" end
+    def white;           "\e[37;1m#{self}\e[0m" end
     
     def bg_black;       "\e[40m#{self}\e[0m" end
     def bg_red;         "\e[41;1m#{self}\e[0m" end
@@ -35,34 +37,41 @@ end
 
 
 class ColourCode
+    
     @codeLength = 4
 
     attr_reader :code
-    def initialize(colourArray)
-        
+    
+    def initialize(colourArray , clue = [".",".",".","."] )
+        @clue = clue
         @code = colourArray
         @peg = "●"
-        @colours = {0 => @peg.red, 1 => @peg.blue, 2 => @peg.yellow, 3 => @peg.green, 4 => @peg.magenta, 5 => @peg.cyan} 
-                
+        @colours = {0 => @peg.red, 1 => @peg.blue, 2 => @peg.yellow, 3 => @peg.green, 4 => @peg.magenta, 5 => @peg.cyan, "R" =>"*".red, "W" => "*".white, "." => "-"} 
     end
 
     def self.buildCode
-       tempList = []
+        tempList = []
         @codeLength.times do
             tempList.push(rand(0..5))
-         end
+        end
         ColourCode.new(tempList)
     end
     
     def returnColouredCode
-         
         c = @code.each.map { |value| @colours[value] }.join(" ")
-        
     end
 
     def correctGuess?(guessArray)
         return true if guessArray == @code
         false
+    end
+
+    def attachClue(clueArray)
+        @clue = clueArray
+    end
+
+    def displayClue
+        @clue.map { |i| @colours[i]}.join(' ')    
     end
 end
 
@@ -73,9 +82,8 @@ class Board
     def initialize
         #codes[0] will always be the correct code.
         @codes = []
-        @turns = 0
-        @codes << ColourCode.buildCode
-        
+        @turns = 10
+        @codes << ColourCode.buildCode        
     end
 
     def submitGuess(guess)
@@ -84,44 +92,60 @@ class Board
             print "Enter to exit..."
             gets
             exit
+        else
+            @codes << ColourCode.new(guess,checkCode(guess))
         end
-
-        @codes << ColourCode.new(guess) 
          
-        
     end
     
     def drawBoard
         system("clear")
         sleep(0.5)
         (0...@codes.length).each do |i|
-            if i == 10
-                
-                puts "\r? ? ? ?"
+            if i == 100
+                puts "\r? ? ? ?\t#{@codes[i].displayClue}"
                 puts "-------"
-               
             else
-                puts "\r#{@codes[i].returnColouredCode}"
+                puts "\r#{@codes[i].returnColouredCode}\t#{@codes[i].displayClue}"
                 puts "-------"
-               
             end
-            
-            
-          end
+        end
     end
 
     def enterGuess
         puts("#{"Colours are".yellow} #{"numbers".green}, #{"numbers".red} are #{"colours".blue}")
         puts("#{"0".red} #{"1".blue} #{"2".yellow} #{"3".green} #{"4".magenta} #{"5".cyan}")
-        print("Enter a guess using the numbers that corrosponde to the colour you want\n:")
+        print("#{@turns} turns\nEnter a guess using the numbers that corrosponde to the colours you want\n:")
         guess = gets.chomp.split('').map(&:to_i)
         submitGuess(guess)
-        
+    end
+
+    def checkCode(guess)
+        ans = []
+        (0...@codes[0].code.length).each do |i|
+           if guess[i] == @codes[0].code[i]
+               ans << "R"
+           else
+               if @codes[0].code.any?guess[i]
+                   ans << "W"
+               end   
+           end
+           
+        end
+        ans
+    end
+
+    def start
+        while @turns > 0
+            drawBoard
+            enterGuess
+            @turns -= 1    
+        end
+        puts "You Lose\nGame #{"Over".red}\n#{@codes[0].returnColouredCode} was the answer"
         
     end
-    
 
-end
+end #class end
 
 
 
@@ -130,7 +154,5 @@ end
 #*********************
 system("clear")
 f = Board.new()
-3.times do 
-    f.drawBoard
-    f.enterGuess
-end
+f.start
+
